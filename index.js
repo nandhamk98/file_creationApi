c449e7108f82c06109cc3abe417f6e220816c3f6
Nandhakumar <nandhakumarmk98@gmail.com>
2022-06-16 21:39:15 +0530

    Added comments for understanding

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
+
+// Inbuilt libraries
 import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
 
+// Setting the applications
 dotenv.config();
 
 const fsPromises = fs.promises;
 const app = express();
 const PORT = process.env.PORT;
 
+// Setting the file/folder path
 const HOME_DIR = os.homedir();
 const CURRENT_DIR = "/Desktop/backupFiles";
 const PATH = path.join(HOME_DIR, CURRENT_DIR);
 
+// Creating the folder for Writing/Reading files
 fs.mkdir(PATH, { recursive: true }, (err) => {
   if (err) throw err;
   console.log("folder created");
 });
 
+// Root endpoint to check the application start
 app.get("/", function (req, res) {
   res.send("Welcome to file creation app");
 });
 
+//Get all the files from backup folder
 app.get("/files", async function (req, res) {
   let data = {};
   try {
@@ -33,6 +40,7 @@ app.get("/files", async function (req, res) {
   res.send(data.files || data.err ? data : { message: "No Files Found" });
 });
 
+//Create a file with timestamp name and timestamp as a data inside the backup folder
 app.post("/create-file", async function (req, res) {
   let now = Date.now();
   let data = {};
@@ -48,6 +56,7 @@ app.post("/create-file", async function (req, res) {
   res.send(data);
 });
 
+// listening to the port
 app.listen(PORT, () => {
   console.log(`The server started in ${PORT}`);
 });
